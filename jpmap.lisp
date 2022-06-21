ddb1f219232351ff28bc7b347c4b22d908ff748e
Higashi, Hiromitsu <supercaveman@gmail.com>
2022-06-21 11:41:03 +0900

    delete
    move to src dir

--- jpmap.lisp
+++
@@ -1,9 +0,0 @@
-(defun main (&rest argv)
-  (cut csv-file :to ht :key '(1 3 5) :val '(6 7))
-  (sets pref-tree (ht2tree ht))
-  (make-prefs-ht)
-  (make-towns-ht ward-ht town-ht))	; => MAIN
-
-(main)					; => 
-
-
